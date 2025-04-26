1a362e41c32e3ef10e6b4f21511c875f67e880cb
rrobedo14 <rrobedo14@gmail.com>
2025-04-25 20:49:31 -0400

    Modified build.sh to add to folder to root dir

--- build.sh
+++ build.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-python3 src/main.py "/StaticSiteGenerator/"
+
+# Generate the site in the project directory
+python3 project/src/main.py "/StaticSiteGenerator/"
+
+# Create docs directory at the root if it doesn't exist
+mkdir -p docs
+
+# Copy everything from project/docs to the root docs directory
+cp -r project/docs/* docs/
+
+# Add .nojekyll file to prevent Jekyll processing 
+touch docs/.nojekyll
+
+echo "Site built and copied to repository root docs directory"
--- project/src/main.py
+++ project/src/main.py
@@ -5,11 +5,24 @@ import sys
 from copystatic import copy_files_recursive
 from generate_content import generate_pages_recursive
 
-dir_path_static = "./static"
-dir_path_public = "./docs"  # Change this line   #"./public"
-dir_path_content = "./content"
-template_path = "./template.html"
+
+# Get the directory where the script is located
+script_dir = os.path.dirname(os.path.abspath(__file__))  # src directory
+project_dir = os.path.dirname(script_dir)  # project directory
+
+# Set paths relative to the project directory
+dir_path_static = os.path.join(project_dir, "static")
+dir_path_public = os.path.join(project_dir, "docs")  
+dir_path_content = os.path.join(project_dir, "content")
+template_path = os.path.join(project_dir, "template.html")
+
 basepath = "/" if len(sys.argv) < 2 else sys.argv[1]
+
+# dir_path_static = "./static"
+# dir_path_public = "./docs"  # Change this line   #"./public"
+# dir_path_content = "./content"
+# template_path = "./template.html"
+# basepath = "/" if len(sys.argv) < 2 else sys.argv[1]
 
 def main():
     print("Deleting public directory..")
