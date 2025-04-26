4826dbe62cd7641cfda40b8b8cc6eb4e23991b25
rrobedo14 <rrobedo14@gmail.com>
2025-04-25 20:58:02 -0400

    Changed main.py to generate files to root/docs only

--- project/src/main.py
+++ project/src/main.py
@@ -6,18 +6,24 @@ from copystatic import copy_files_recursive
 from generate_content import generate_pages_recursive
 
 
-# Get the directory where the script is located
+import os
+import sys
+import shutil
+
+# Get the absolute path to the script directory
 script_dir = os.path.dirname(os.path.abspath(__file__))  # src directory
 project_dir = os.path.dirname(script_dir)  # project directory
+repo_root = os.path.dirname(project_dir)  # repository root
 
-# Set paths relative to the project directory
+# Set paths relative to the project directory for content and templates
 dir_path_static = os.path.join(project_dir, "static")
-dir_path_public = os.path.join(project_dir, "docs")  
 dir_path_content = os.path.join(project_dir, "content")
 template_path = os.path.join(project_dir, "template.html")
 
-basepath = "/" if len(sys.argv) < 2 else sys.argv[1]
+# Set docs path to repository root's docs directory
+dir_path_public = os.path.join(repo_root, "docs")
 
+basepath = "/" if len(sys.argv) < 2 else sys.argv[1]
 # dir_path_static = "./static"
 # dir_path_public = "./docs"  # Change this line   #"./public"
 # dir_path_content = "./content"
